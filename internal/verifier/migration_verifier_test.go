a94165536df1d8ec28829916a1ddbf6f2db43c48
Alexander Neben <alexander.neben@mongodb.com>
2022-08-19 12:56:26 -0700

    Made some test changes to test patch builds and the commit queue (#2)

--- internal/verifier/migration_verifier_test.go
+++ internal/verifier/migration_verifier_test.go
@@ -7,6 +7,13 @@ import (
 func TestExample(t *testing.T) {
 	var a int = 0
 	if a != 0 {
-		t.Errorf("a should b 0")
+		t.Errorf("a should be 0")
+	}
+}
+
+func TestExample2(t *testing.T) {
+	var a int = 1
+	if a != 1 {
+		t.Errorf("a should be 1")
 	}
 }
