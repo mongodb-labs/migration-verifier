c35b1881fbfcb4cc0d041518915fcab522749e65
Felipe Gasper <felipe@felipegasper.com>
2024-11-06 12:31:05 -0500

    rename test

--- internal/verifier/reset_test.go
+++ internal/verifier/reset_test.go
@@ -2,9 +2,13 @@ package verifier
 
 import (
 	"context"
+	"strings"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/10gen/migration-verifier/internal/partitions"
 )
 
 func (suite *MultiDataVersionTestSuite) TestResetPrimaryTask() {
@@ -36,7 +40,9 @@ func (suite *MultiDataVersionTestSuite) TestResetPrimaryTask() {
 	suite.Assert().Len(taskDocs, 1)
 }
 
-func (suite *MultiDataVersionTestSuite) TestResetCollectionTasks() {
+func (suite *MultiDataVersionTestSuite) TestResetNonPrimaryTasks() {
+	ctx := context.Background()
+
 	verifier := buildVerifier(suite.T(), suite.srcMongoInstance, suite.dstMongoInstance, suite.metaMongoInstance)
 
 	// Create a primary task, and set it to complete.
@@ -59,28 +65,39 @@ func (suite *MultiDataVersionTestSuite) TestResetCollectionTasks() {
 		verifier.UpdateVerificationTask(collTask),
 	)
 
-	// Create four partition tasks: three with the same namespace as the
+	// Create three partition tasks with the same namespace as the
 	// collection-verification task (status=[added, processing, completed]),
 	// and another for a different namespace that’s completed.
-	for _, status := range []verificationTaskStatus{
-		verificationTaskAdded,
-		verificationTaskProcessing,
-		verificationTaskCompleted,
+	for _, taskParts := range []struct {
+		Status    verificationTaskStatus
+		Namespace string
+	}{
+		{verificationTaskAdded, ns1},
+		{verificationTaskProcessing, ns1},
+		{verificationTaskCompleted, ns1},
+		{verificationTaskAdded, ns2},
+		{verificationTaskProcessing, ns2},
+		{verificationTaskCompleted, ns2},
 	} {
 		task, err := verifier.InsertPartitionVerificationTask(
 			&partitions.Partition{
-				Ns: &partition.NameSpace{
-					DB:   "foo",
-					Coll: "bar",
+				Ns: &partitions.Namespace{
+					DB:   strings.Split(taskParts.Namespace, ".")[0],
+					Coll: strings.Split(taskParts.Namespace, ".")[1],
 				},
 			},
 			nil,
-			"foo.bar",
+			taskParts.Namespace,
+		)
+		suite.Require().NoError(err)
+
+		task.Status = taskParts.Status
+		suite.Require().NoError(
+			verifier.UpdateVerificationTask(task),
 		)
 	}
 
-	ctx := context.Background()
-
+	// Reset tasks
 	err = verifier.doInMetaTransaction(
 		ctx,
 		func(_ context.Context, ctx mongo.SessionContext) error {
@@ -89,11 +106,84 @@ func (suite *MultiDataVersionTestSuite) TestResetCollectionTasks() {
 	)
 	suite.Require().NoError(err)
 
+	// Contents should just be the primary task and
+	// the completed partition-level.
 	tasksColl := verifier.verificationTaskCollection()
-	cursor, err := tasksColl.Find(ctx, bson.M{})
+	cursor, err := tasksColl.Find(
+		ctx,
+		bson.M{},
+		options.Find().SetSort(bson.D{
+			{"type", 1},
+			{"query_filter.namespace", 1},
+			{"status", 1},
+		}),
+	)
 	suite.Require().NoError(err)
-	var taskDocs []bson.M
+	var taskDocs []VerificationTask
 	suite.Require().NoError(cursor.All(ctx, &taskDocs))
 
-	suite.Assert().Len(taskDocs, 1)
+	suite.Require().Len(taskDocs, 5)
+
+	// The tasks that should remain are:
+
+	// the primary (completed)
+	suite.Assert().Equal(
+		verificationTaskPrimary,
+		taskDocs[0].Type,
+	)
+
+	// the 2 ns2 partition tasks that weren’t completed (both “added”)
+	suite.Assert().Equal(
+		verificationTaskVerifyDocuments,
+		taskDocs[1].Type,
+	)
+	suite.Assert().Equal(
+		verificationTaskAdded,
+		taskDocs[1].Status,
+	)
+	suite.Assert().Equal(
+		ns2,
+		taskDocs[1].QueryFilter.Namespace,
+	)
+
+	suite.Assert().Equal(
+		verificationTaskVerifyDocuments,
+		taskDocs[2].Type,
+	)
+	suite.Assert().Equal(
+		verificationTaskAdded,
+		taskDocs[2].Status,
+	)
+	suite.Assert().Equal(
+		ns2,
+		taskDocs[2].QueryFilter.Namespace,
+	)
+
+	// the ns2 partition task that *was* completed
+	suite.Assert().Equal(
+		verificationTaskVerifyDocuments,
+		taskDocs[3].Type,
+	)
+	suite.Assert().Equal(
+		verificationTaskCompleted,
+		taskDocs[3].Status,
+	)
+	suite.Assert().Equal(
+		ns2,
+		taskDocs[3].QueryFilter.Namespace,
+	)
+
+	// ns1’s verify-collection task (added state)
+	suite.Assert().Equal(
+		verificationTaskVerifyCollection,
+		taskDocs[4].Type,
+	)
+	suite.Assert().Equal(
+		verificationTaskAdded,
+		taskDocs[4].Status,
+	)
+	suite.Assert().Equal(
+		ns1,
+		taskDocs[4].QueryFilter.Namespace,
+	)
 }
