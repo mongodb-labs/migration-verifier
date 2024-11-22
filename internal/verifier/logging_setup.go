fce41232cb822313a220bb4263fd8f531981eac1
Felipe Gasper <felipe@felipegasper.com>
2024-11-22 14:33:29 -0500

    revert

--- internal/verifier/logging_setup.go
+++ internal/verifier/logging_setup.go
@@ -7,10 +7,9 @@ import (
 	"github.com/10gen/migration-verifier/internal/logger"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
-	"golang.org/x/term"
 )
 
-func getLogWriter(logPath string) (io.Writer, io.Writer) {
+func getLogWriter(logPath string) io.Writer {
 	var writer io.Writer
 
 	switch logPath {
@@ -22,34 +21,23 @@ func getLogWriter(logPath string) (io.Writer, io.Writer) {
 
 	default:
 		if w, err := logger.NewRotatingWriter(logPath); err == nil {
-			return w, w
+			return w
 		}
 
 		log.Fatal().Msgf("Failed to open logPath: %s", logPath)
 	}
 
-	return writer, zerolog.SyncWriter(writer)
+	return zerolog.SyncWriter(writer)
 }
 
 func getLoggerAndWriter(logPath string) (*logger.Logger, io.Writer) {
-	rawWriter, writer := getLogWriter(logPath)
+	writer := getLogWriter(logPath)
 
 	consoleWriter := zerolog.ConsoleWriter{
 		Out:        writer,
 		TimeFormat: timeFormat,
-		NoColor:    shouldSuppressColor(rawWriter),
 	}
 
 	l := zerolog.New(consoleWriter).With().Timestamp().Logger()
 	return logger.NewLogger(&l, writer), writer
-}
-
-// Returns true unless the writer is a TTY.
-func shouldSuppressColor(writer io.Writer) bool {
-	osFile, isOsFile := writer.(*os.File)
-	if !isOsFile {
-		return true
-	}
-
-	return !term.IsTerminal(int(osFile.Fd()))
 }
