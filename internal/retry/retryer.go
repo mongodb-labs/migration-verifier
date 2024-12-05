5dfafab93e0daccbd3b2ab3f1b58068e241b409a
Felipe Gasper <felipe@felipegasper.com>
2024-12-05 11:11:01 -0500

    real clones

--- internal/retry/retryer.go
+++ internal/retry/retryer.go
@@ -2,6 +2,7 @@ package retry
 
 import (
 	"fmt"
+	"slices"
 	"time"
 
 	"github.com/10gen/migration-verifier/option"
@@ -15,14 +16,13 @@ type retryCallbackInfo struct {
 // Retryer handles retrying operations that fail because of network failures.
 type Retryer struct {
 	retryLimit           time.Duration
-	retryRandomly        bool
 	before               option.Option[func()]
 	callbacks            []retryCallbackInfo
 	description          option.Option[string]
 	additionalErrorCodes []int
 }
 
-// New returns a new retryer.
+// New returns a new Retryer with DefaultDurationLimit as its time limit.
 func New() *Retryer {
 	return &Retryer{
 		retryLimit: DefaultDurationLimit,
@@ -30,47 +30,51 @@ func New() *Retryer {
 }
 
 // WithErrorCodes returns a new Retryer that will retry on the codes passed to
-// this method. This allows for a single function to customize the codes it
+// this method. This allows for a single retryer to customize the codes it
 // wants to retry on. Note that if the Retryer already has additional custom
 // error codes set, these are _replaced_ when this method is called.
 func (r *Retryer) WithErrorCodes(codes ...int) *Retryer {
-	r2 := *r
+	r2 := r.clone()
 	r2.additionalErrorCodes = codes
 
-	return &r2
+	return r2
 }
 
+// WithRetryLimit returns a new retryer with the specified time limit.
 func (r *Retryer) WithRetryLimit(limit time.Duration) *Retryer {
-	r2 := *r
+	r2 := r.clone()
 	r2.retryLimit = limit
 
-	return &r2
+	return r2
 }
 
-// WithBefore sets a callback that always runs before any retryer callback.
+// WithBefore returns a new retryer with a callback that always runs before
+// any retryer callback.
 //
 // This is useful if there are multiple callbacks and you need to reset some
 // condition before each retryer iteration. (In the single-callback case it’s
 // largely redundant.)
 func (r *Retryer) WithBefore(todo func()) *Retryer {
-	r2 := *r
+	r2 := r.clone()
 	r2.before = option.Some(todo)
 
-	return &r2
+	return r2
 }
 
+// WithDescription returns a new retryer with the given description.
 func (r *Retryer) WithDescription(msg string, args ...any) *Retryer {
-	r2 := *r
+	r2 := r.clone()
 	r2.description = option.Some(fmt.Sprintf(msg, args...))
 
-	return &r2
+	return r2
 }
 
+// WithCallback returns a new retryer with the additional callback.
 func (r *Retryer) WithCallback(
 	callback RetryCallback,
 	msg string, args ...any,
 ) *Retryer {
-	r2 := *r
+	r2 := r.clone()
 
 	r2.callbacks = append(
 		r2.callbacks,
@@ -79,6 +83,17 @@ func (r *Retryer) WithCallback(
 			description: fmt.Sprintf(msg, args...),
 		},
 	)
+
+	return r2
+}
+
+func (r *Retryer) clone() *Retryer {
+	r2 := *r
+
+	r2.before = option.FromPointer(r.before.ToPointer())
+	r2.description = option.FromPointer(r.description.ToPointer())
+	r2.callbacks = slices.Clone(r.callbacks)
+	r2.additionalErrorCodes = slices.Clone(r.additionalErrorCodes)
 
 	return &r2
 }
