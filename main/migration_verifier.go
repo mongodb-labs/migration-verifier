1f2df03a86f04d82e526ee728a9d03f8a91721da
Felipe Gasper <felipe@felipegasper.com>
2025-02-27 16:33:00 -0500

    forbid TODO

--- main/migration_verifier.go
+++ main/migration_verifier.go
@@ -51,6 +51,8 @@ func main() {
 	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
 
 	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+
+	//nolint:forbidigo
 	ctx := contextplus.New(context.TODO())
 
 	flags := []cli.Flag{
