d6cbb2aee5ccec6f04acc63fdae716e23f94a260
Felipe Gasper <felipe@felipegasper.com>
2025-03-26 14:37:42 -0400

    save

--- internal/reportutils/reportutils_test.go
+++ internal/reportutils/reportutils_test.go
@@ -1,6 +1,7 @@
 package reportutils
 
 import (
+	"math"
 	"testing"
 	"time"
 
@@ -92,6 +93,7 @@ func (s *UnitTestSuite) TestBytesToUnit() {
 		{1024, KiB, "1"},
 		{1124, KiB, "1.1"},
 		{1124000, KiB, "1,097.66"},
+		{math.MaxUint64, Bytes, "123123"},
 	}
 
 	for _, tt := range tests {
