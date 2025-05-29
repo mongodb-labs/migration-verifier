92e266d22fbce193c7c76e286d807f37e1898767
Ivan Huang <ivan@riversense.tw>
2025-05-30 02:08:00 +0800

    Add shell script parameters to support build for different architectures or operating systems.

--- build.sh
+++ build.sh
@@ -1,6 +1,38 @@
 #!/bin/sh
 
+usage() {
+cat << EOF
+Usage: ./build.sh [-a <386|amd64|arm|arm64>] [-o <linux|darwin|windows>]
+Build migration-verifier for mongosync.
+
+-a          Specify the target architecture for the build, if not specified, it will use the current architecture.
+-o          Specify the target operating system for the build, if not specified, it will use the current OS.
+-h          Display this help message.
+EOF
+exit 1;
+}
+
+while getopts "o:a:" option; do
+    case "$option" in
+        a)
+            arch=${OPTARG}
+            test "$arch" = "386" -o "$arch" = amd64 -o "$arch" = "arm" -o "$arch" = "arm64" || usage
+            ;;
+        o)
+            os=${OPTARG}
+            test "$os" = "linux" -o "$os" = "darwin" -o "$os" = "windows" || usage
+            ;;
+        h)
+            usage
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
 commit=$(git show --no-patch --format='%H')
 buildTime=$(date -u)
 
-go build -ldflags="-X 'main.Revision=$commit' -X 'main.BuildTime=$buildTime'" main/migration_verifier.go
+GOARCH=${arch:=$(go env GOARCH)} GOOS=${os:=$(go env GOOS)} go build -ldflags="-X 'main.Revision=$commit' -X 'main.BuildTime=$buildTime'" main/migration_verifier.go
