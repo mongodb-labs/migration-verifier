98f61fbce055041274bc84696d6025f896cdd988
Felipe Gasper <felipe@felipegasper.com>
2025-06-23 13:33:12 -0400

    Log the Git commit & time when building.

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@
 commit=$(git show --no-patch --format='%H')
 buildTime=$(date -u)
 
+printf 'Building migration-verifier …\n\tcommit: %s\n\tbuildTime: %s\n' "$commit" "$buildTime"
+
 go build -ldflags="-X 'main.Revision=$commit' -X 'main.BuildTime=$buildTime'" main/migration_verifier.go
