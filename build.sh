fba48a6c091372876d843cb7945d7f2f5cb79c7e
Felipe Gasper <felipe@felipegasper.com>
2025-07-07 15:52:13 -0400

    better build

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-revision=$(git describe --tags --exact-match || git show --no-patch --format='%H')
+revision=$(git describe --tags --exact-match || echo "DEVELOPMENT:"; git show --no-patch --format='%H')
 buildTime=$(date -u)
 
-printf 'Building migration-verifier …\n\tcommit: %s\n\tbuildTime: %s\n' "$revision" "$buildTime"
+goos=$(go env GOOS)
+goarch=$(go env GOARCH)
+
+printf 'Building migration-verifier for %s/%s …\n' "$goos" "$goarch"
+printf '\trevision: %s\n' "$revision"
+printf '\tbuildTime: %s\n' "$buildTime"
 
 go build -ldflags="-X 'main.Revision=$revision' -X 'main.BuildTime=$buildTime'" main/migration_verifier.go
