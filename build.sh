c974b5a40b02c4707baa7d54125206260cace6b2
Felipe Gasper <felipe@felipegasper.com>
2025-07-07 16:17:16 -0400

    polish

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ goos=$(go env GOOS)
 goarch=$(go env GOARCH)
 
 printf 'Building migration-verifier for %s/%s …\n' "$goos" "$goarch"
-printf '\trevision: %s\n' "$revision"
-printf '\tbuildTime: %s\n' "$buildTime"
+printf '\tRevision: %s\n' "$revision"
+printf '\tBuild Time: %s\n' "$buildTime"
 
 go build -ldflags="-X 'main.Revision=$revision' -X 'main.BuildTime=$buildTime'" main/migration_verifier.go
