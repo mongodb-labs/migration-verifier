916bad0ee16a13117028f73338f9d64f9738c4a4
Felipe Gasper <FGasper@users.noreply.github.com>
2025-07-08 12:10:46 -0400

    REP-6296 Add a GitHub release process (#121)
    This:
    - adds a GitHub Action that runs whenever a `v`-prefixed semver tag is pushed
    - adds documentation for the release process
    - reworks the `build.sh` script to prefer the release version rather than the git revision

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-commit=$(git show --no-patch --format='%H')
+revision=$(git describe --tags --exact-match 2>/dev/null || echo "DEVELOPMENT:$(git describe --tags)")
 buildTime=$(date -u)
 
-printf 'Building migration-verifier …\n\tcommit: %s\n\tbuildTime: %s\n' "$commit" "$buildTime"
+goos=$(go env GOOS)
+goarch=$(go env GOARCH)
 
-go build -ldflags="-X 'main.Revision=$commit' -X 'main.BuildTime=$buildTime'" main/migration_verifier.go
+printf 'Building migration-verifier for %s/%s …\n' "$goos" "$goarch"
+printf '\tRevision: %s\n' "$revision"
+printf '\tBuild Time: %s\n' "$buildTime"
+
+go build -ldflags="-X 'main.Revision=$revision' -X 'main.BuildTime=$buildTime'" main/migration_verifier.go
