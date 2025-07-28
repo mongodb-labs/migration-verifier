ae89780de408be0718bbd548d6507e5bb67ff920
Felipe Gasper <felipe@felipegasper.com>
2025-07-28 16:46:12 -0400

    add file

---
+++ internal/verifier/comparison.go
@@ -0,0 +1,36 @@
+package verifier
+
+import mapset "github.com/deckarep/golang-set/v2"
+
+type DocCompareMethod string
+type DocQueryFunction string
+
+const (
+	DocCompareBinary           DocCompareMethod = "binary"
+	DocCompareIgnoreOrder      DocCompareMethod = "ignoreOrder"
+	DocCompareToHashedIndexKey DocCompareMethod = "toHashedIndexKey"
+
+	DocQueryFunctionFind      DocQueryFunction = "find"
+	DocQueryFunctionAggregate DocQueryFunction = "aggregate"
+)
+
+var DocCompareMethods = mapset.NewSet(
+	DocCompareBinary,
+	DocCompareIgnoreOrder,
+	DocCompareToHashedIndexKey,
+)
+
+func (dcm DocCompareMethod) ShouldIgnoreFieldOrder() bool {
+	return dcm == DocCompareIgnoreOrder
+}
+
+func (dcm DocCompareMethod) QueryFunction() DocQueryFunction {
+	switch dcm {
+	case DocCompareBinary, DocCompareIgnoreOrder:
+		return DocQueryFunctionFind
+	case DocCompareToHashedIndexKey:
+		return DocQueryFunctionAggregate
+	default:
+		panic("Unknown doc compare method: " + dcm)
+	}
+}
