mongodb-labs/migration-verifier

commit b4467f7a4f01dc6ca5ce7c7935dac1dcb27966c8

Avoid “decreasing” source docs count in recheck generations.
The aggregation to compile namespace stats assumes that
`source_documents_count` always represents the number of documents to
expect on the source. For recheck tasks, though, once finished the verifier
sets that field to the number of documents actually _found_ on the source.
Thus, as the verifier finishes tasks in recheck generations, the total
number of source documents shown in the progress reports decreases. This
is confusing.

This changeset rectifies that by always using the size of the `_ids` array
to count the # of source documents to report in recheck generations’s
progress reports.

internal/verifier/statistics.go
@@ -115,7 +115,10 @@ const perNsStatsPipelineTemplate = `[
 				verify-collection tasks.
 
 				In later generations we don’t have verify-collection tasks,
-				so we add up the individual recheck batch tasks.
+				so we add up the individual recheck batch tasks. Note that,
+				in these tasks source_documents_count refers to the actual
+				number of docs found on the source, not all the documents
+				checked.
 			*/}}
 			"totalDocs": {
 				"$cond": [
@@ -126,7 +129,11 @@ const perNsStatsPipelineTemplate = `[
 							{ "$ne": [ "$generation", 0 ] }
 						] }
 					] },
-					"$source_documents_count",
+					{ "$cond": [
+						{"$eq": [ "$generation", 0 ]},
+						"$source_documents_count",
+						{ "$size": "$_ids" }
+					] },
 					0
 				]
 			},

commit fb36616066cc1c62b0b35f845fc3e59ff51cefca

Fix $size error in statistics computation.
Commit b4467f7a broke statistics computation when a recheck task contains
no document IDs because in that case the `_ids` field may not be an array.

This fixes that by checking the type before deciding whether to use
{ $size: "$_ids" } or $source_document_count.

internal/verifier/statistics.go
@@ -121,21 +121,24 @@ const perNsStatsPipelineTemplate = `[
 				checked.
 			*/}}
 			"totalDocs": {
-				"$cond": [
-					{ "$or": [
+				"$cond": {
+					"if": { "$or": [
 						{ "$eq": [ "$type", "{{.VerifyCollType}}" ] },
 						{ "$and": [
 							{ "$eq": [ "$type", "{{.VerifyDocsType}}" ] },
 							{ "$ne": [ "$generation", 0 ] }
 						] }
 					] },
-					{ "$cond": [
-						{"$eq": [ "$generation", 0 ]},
-						"$source_documents_count",
-						{ "$size": "$_ids" }
-					] },
-					0
-				]
+					"then": { "$cond": {
+						"if": { "$and": [
+							{ "$gt": [ "$generation", 0 ] },
+							{ "$eq": [ "array", { "$type": "$_ids" } ] }
+						] },
+						"then": { "$size": "$_ids" },
+						"else": "$source_documents_count"
+					} },
+					"else": 0
+				}
 			},
 
 			{{/*