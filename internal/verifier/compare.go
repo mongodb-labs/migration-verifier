812dc46321651cc3dc36f762d4de15ec87a01e67
Felipe Gasper <felipe@felipegasper.com>
2025-11-03 20:36:06 -0500

    revert read pref

--- internal/verifier/compare.go
+++ internal/verifier/compare.go
@@ -609,7 +609,6 @@ func (verifier *Verifier) getDocumentsCursor(
 ) (*cursor.BatchCursor, error) {
 	var findOptions bson.D
 	runCommandOptions := options.RunCmd()
-	runCommandOptions.SetReadPreference(verifier.readPreference)
 	var andPredicates bson.A
 
 	var aggOptions bson.D
