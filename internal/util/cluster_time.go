e2ad320837209bbe99b4985f36e8d13e704d5d2f
Felipe Gasper <felipe@felipegasper.com>
2025-11-07 20:42:24 -0500

    cluster time optimization

--- internal/util/cluster_time.go
+++ internal/util/cluster_time.go
@@ -1,23 +1,19 @@
 package util
 
 import (
+	"github.com/10gen/migration-verifier/mbson"
 	"github.com/pkg/errors"
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 func GetClusterTimeFromSession(sess *mongo.Session) (bson.Timestamp, error) {
-	ctStruct := struct {
-		ClusterTime struct {
-			ClusterTime bson.Timestamp `bson:"clusterTime"`
-		} `bson:"$clusterTime"`
-	}{}
-
 	clusterTimeRaw := sess.ClusterTime()
-	err := bson.Unmarshal(sess.ClusterTime(), &ctStruct)
+
+	ctrv, err := clusterTimeRaw.LookupErr("$clusterTime", "clusterTime")
 	if err != nil {
-		return bson.Timestamp{}, errors.Wrapf(err, "failed to find clusterTime in session cluster time document (%v)", clusterTimeRaw)
+		return bson.Timestamp{}, errors.Wrapf(err, "finding clusterTime in session cluster time document (%v)", clusterTimeRaw)
 	}
 
-	return ctStruct.ClusterTime.ClusterTime, nil
+	return mbson.CastRawValue[bson.Timestamp](ctrv)
 }
