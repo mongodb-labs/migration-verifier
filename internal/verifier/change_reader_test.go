7e917b3ad97e8da830da843b04a8178dcca6edc8
Felipe Gasper <felipe@felipegasper.com>
2025-11-24 13:01:06 -0500

    avoid sharded w/ oplog

--- internal/verifier/change_reader_test.go
+++ internal/verifier/change_reader_test.go
@@ -1,8 +1,14 @@
 package verifier
 
+import "github.com/10gen/migration-verifier/internal/util"
+
 // TestFailChangeReaderOptChange confirms that verifier fails if it restarts
 // with different change-reader settings.
 func (suite *IntegrationTestSuite) TestFailChangeReaderOptChange() {
+	if suite.GetTopology(suite.srcMongoClient) == util.TopologySharded {
+		suite.T().Skipf("sharded source can only read changes via change stream")
+	}
+
 	ctx := suite.Context()
 
 	v1 := suite.BuildVerifier()
@@ -31,6 +37,10 @@ func (suite *IntegrationTestSuite) TestFailChangeReaderOptChange() {
 
 	suite.Require().Error(err, "wrong source opt should fail")
 	suite.Assert().ErrorAs(err, &changeReaderOptMismatchErr{})
+
+	if suite.GetTopology(suite.dstMongoClient) == util.TopologySharded {
+		return
+	}
 
 	badDstOptVerifier := suite.BuildVerifier()
 	suite.Require().NoError(
