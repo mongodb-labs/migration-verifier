93b67d8baa7e2c4dbc99ec3a2f77bd3a2f7035b1
Felipe Gasper <felipe@felipegasper.com>
2025-11-24 12:06:23 -0500

    confirm failure

---
+++ internal/verifier/change_reader_test.go
@@ -0,0 +1,48 @@
+package verifier
+
+// TestFailChangeReaderOptChange confirms that verifier fails if it restarts
+// with different change-reader settings.
+func (suite *IntegrationTestSuite) TestFailChangeReaderOptChange() {
+	ctx := suite.Context()
+
+	v1 := suite.BuildVerifier()
+	suite.Require().NoError(
+		v1.SetSrcChangeReaderMethod(ChangeReaderOptChangeStream),
+	)
+	suite.Require().NoError(
+		v1.SetDstChangeReaderMethod(ChangeReaderOptChangeStream),
+	)
+
+	v1Runner := RunVerifierCheck(ctx, suite.T(), v1)
+	suite.Require().NoError(
+		v1Runner.AwaitGenerationEnd(),
+	)
+
+	badSrcOptVerifier := suite.BuildVerifier()
+	suite.Require().NoError(
+		badSrcOptVerifier.SetSrcChangeReaderMethod(ChangeReaderOptOplog),
+	)
+	suite.Require().NoError(
+		badSrcOptVerifier.SetDstChangeReaderMethod(ChangeReaderOptChangeStream),
+	)
+
+	err := RunVerifierCheck(ctx, suite.T(), badSrcOptVerifier).
+		AwaitGenerationEnd()
+
+	suite.Require().Error(err, "wrong source opt should fail")
+	suite.Assert().ErrorAs(err, &changeReaderOptMismatchErr{})
+
+	badDstOptVerifier := suite.BuildVerifier()
+	suite.Require().NoError(
+		badSrcOptVerifier.SetSrcChangeReaderMethod(ChangeReaderOptChangeStream),
+	)
+	suite.Require().NoError(
+		badSrcOptVerifier.SetDstChangeReaderMethod(ChangeReaderOptOplog),
+	)
+
+	err = RunVerifierCheck(ctx, suite.T(), badDstOptVerifier).
+		AwaitGenerationEnd()
+
+	suite.Require().Error(err, "wrong destination opt should fail")
+	suite.Assert().ErrorAs(err, &changeReaderOptMismatchErr{})
+}
