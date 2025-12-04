e9589bfe2b5aacc7fc18f1a81a38030a7b765d2f
Felipe Gasper <felipe@felipegasper.com>
2025-12-04 12:59:32 -0500

    add test of GetBatch

--- mmongo/cursor_all_test.go
+++ mmongo/cursor_all_test.go
@@ -10,7 +10,73 @@ import (
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"go.mongodb.org/mongo-driver/v2/mongo/readconcern"
+	"go.mongodb.org/mongo-driver/v2/mongo/writeconcern"
 )
+
+func TestGetBatch(t *testing.T) {
+	ctx := t.Context()
+
+	connStr := os.Getenv("MVTEST_META")
+	if connStr == "" {
+		t.Skipf("No MVTEST_META found; skipping.")
+	}
+
+	client, err := mongo.Connect(
+		options.Client().ApplyURI(connStr),
+	)
+	require.NoError(t, err)
+
+	coll := client.Database(t.Name()).Collection(
+		"coll",
+		options.Collection().
+			SetWriteConcern(writeconcern.Majority()).
+			SetReadConcern(readconcern.Majority()),
+	)
+
+	sess, err := client.StartSession(options.Session().SetCausalConsistency(true))
+	require.NoError(t, err)
+
+	sctx := mongo.NewSessionContext(ctx, sess)
+
+	docsCount := 1_000
+	const batchSize = 100
+
+	_, err = coll.InsertMany(
+		sctx,
+		lo.RepeatBy(
+			docsCount,
+			func(index int) any {
+				return bson.D{}
+			},
+		),
+	)
+	require.NoError(t, err)
+
+	cursor, err := coll.Find(
+		ctx,
+		bson.D{},
+		options.Find().SetBatchSize(batchSize),
+	)
+	require.NoError(t, err)
+
+	cursor.SetBatchSize(batchSize)
+
+	var docs []bson.Raw
+	var buf []byte
+	for range docsCount / batchSize {
+		docs = docs[:0]
+		buf = buf[:0]
+
+		docs, buf, err = GetBatch(ctx, cursor, docs, buf)
+		require.NoError(t, err)
+
+		assert.Len(t, docs, 100, "should get expected batch")
+	}
+
+	assert.False(t, cursor.TryNext(ctx), "cursor should be done")
+	require.NoError(t, cursor.Err(), "should be no error")
+}
 
 func TestUnmarshalCursor(t *testing.T) {
 	ctx := t.Context()
