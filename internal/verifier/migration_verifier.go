204dc85133f4ca01d8b1f1b429e4c152a891767e
Felipe Gasper <felipe@felipegasper.com>
2025-12-04 13:51:12 -0500

    Revert "remove trace-level command monitor"
    This reverts commit d9294f241f2f1ba9d7c9a58eddb9285928576948.

--- internal/verifier/migration_verifier.go
+++ internal/verifier/migration_verifier.go
@@ -89,7 +89,6 @@ type Verifier struct {
 	lastGeneration     bool
 	running            bool
 	generation         int
-	phase              string
 	port               int
 	metaURI            string
 	metaClient         *mongo.Client
@@ -181,7 +180,6 @@ func NewVerifier(settings VerifierSettings, logPath string) *Verifier {
 		logger: logger,
 		writer: logWriter,
 
-		phase:                Idle,
 		numWorkers:           NumWorkers,
 		readPreference:       readpref.Primary(),
 		partitionSizeInBytes: 400 * 1024 * 1024,
@@ -1255,8 +1253,16 @@ func (verifier *Verifier) verifyMetadataAndPartitionCollection(
 }
 
 func (verifier *Verifier) GetVerificationStatus(ctx context.Context) (*VerificationStatus, error) {
-	taskCollection := verifier.verificationTaskCollection()
 	generation, _ := verifier.getGeneration()
+
+	return verifier.getVerificationStatusForGeneration(ctx, generation)
+}
+
+func (verifier *Verifier) getVerificationStatusForGeneration(
+	ctx context.Context,
+	generation int,
+) (*VerificationStatus, error) {
+	taskCollection := verifier.verificationTaskCollection()
 
 	var results []bson.Raw
 
@@ -1394,18 +1400,6 @@ func (verifier *Verifier) dstClientCollectionByNameSpace(namespace string) *mong
 func (verifier *Verifier) StartServer() error {
 	server := NewWebServer(verifier.port, verifier, verifier.logger)
 	return server.Run(context.Background())
-}
-
-func (verifier *Verifier) GetProgress(ctx context.Context) (Progress, error) {
-	status, err := verifier.GetVerificationStatus(ctx)
-	if err != nil {
-		return Progress{Error: err}, err
-	}
-	return Progress{
-		Phase:      verifier.phase,
-		Generation: verifier.generation,
-		Status:     status,
-	}, nil
 }
 
 // Returned boolean indicates that namespaces are cached, and
