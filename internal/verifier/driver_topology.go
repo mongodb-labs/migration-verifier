a9617d1bfcfc99557d46b365e2981e177d50cae4
Felipe Gasper <felipe@felipegasper.com>
2025-12-08 16:15:10 -0500

    add comment

--- internal/verifier/driver_topology.go
+++ internal/verifier/driver_topology.go
@@ -3,7 +3,10 @@ package verifier
 import "go.mongodb.org/mongo-driver/v2/x/mongo/driver/topology"
 
 func init() {
-	topology.MinSupportedMongoDBVersion = "4.0"
 
+	// Version 2.4 of the driver dropped support for server 4.0.
+	// This is a workaround from GODRIVER-3701 that the maintainers
+	// indicate will preserve the needed functionality.
+	topology.MinSupportedMongoDBVersion = "4.0"
 	topology.SupportedWireVersions.Min = 7 // 4.0
 }
