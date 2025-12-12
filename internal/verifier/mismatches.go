367898b027bd5a79ded35f4480fdc5f46a9ae8e0
Felipe Gasper <felipe@felipegasper.com>
2025-12-12 16:20:03 -0500

    fix typo

--- internal/verifier/mismatches.go
+++ internal/verifier/mismatches.go
@@ -184,7 +184,7 @@ func getDocumentMismatchReportData(
 			{"task", bson.D{{"$in", taskIDs}}},
 		}}},
 		{{"$sort", bson.D{
-			{"detail.mismatchHistory.duration", -1},
+			{"detail.mismatchHistory.durationMS", -1},
 			{"detail.id", 1},
 		}}},
 		{{"$facet", bson.D{
