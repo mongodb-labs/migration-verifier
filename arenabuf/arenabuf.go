b97ddc44f2e1ce1c86c694471099859c5be1fa50
Felipe Gasper <felipe@felipegasper.com>
2026-03-02 15:48:27 -0500

    tweak

--- arenabuf/arenabuf.go
+++ arenabuf/arenabuf.go
@@ -16,13 +16,12 @@ type Buffer[T bufferType] struct {
 func (b *Buffer[T]) Add(in T) T {
 	start := len(b.buf)
 	b.buf = append(b.buf, in...)
-	newBuf := b.buf[start:]
 
-	return newBuf
+	return b.buf[start:]
 }
 
 // Reset creates a new backing buffer. To minimize allocations on subsequent
-// usage it makes the new buffer the same size as the existing one.
+// Add() calls, this makes the new buffer the same size as the existing one.
 func (b *Buffer[T]) Reset() {
 	b.buf = make([]byte, 0, len(b.buf))
 }
