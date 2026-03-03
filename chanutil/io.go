1feb2b3d7e38084d7a5f3fb704695938ef533e68
Felipe Gasper <felipe@felipegasper.com>
2026-03-03 13:53:14 -0500

    fix test

--- chanutil/io.go
+++ chanutil/io.go
@@ -14,14 +14,24 @@ import (
 // returns the value. If the channel was closed, the return value is None.
 func ReadWithDoneCheck[T any](ctx context.Context, ch <-chan T) (option.Option[T], error) {
 	select {
-	case <-ctx.Done():
-		return option.None[T](), util.WrapCtxErrWithCause(ctx)
 	case val, ok := <-ch:
 		if ok {
 			return option.Some(val), nil
 		}
 
 		return option.None[T](), nil
+	default:
+	}
+
+	select {
+	case val, ok := <-ch:
+		if ok {
+			return option.Some(val), nil
+		}
+
+		return option.None[T](), nil
+	case <-ctx.Done():
+		return option.None[T](), util.WrapCtxErrWithCause(ctx)
 	}
 }
 
@@ -30,10 +40,21 @@ func ReadWithDoneCheck[T any](ctx context.Context, ch <-chan T) (option.Option[T
 // an error from the `ctx.Done()` channel, it returns the value of `Err(ctx)`, which includes the
 // cancellation cause. If it writes a value to that channel, it returns `nil`.
 func WriteWithDoneCheck[T any](ctx context.Context, ch chan<- T, val T) error {
+	// 1. Try to send immediately.
+	// If there is room on the channel, this succeeds and returns nil.
+	// If the channel is blocked, the 'default' case catches it so we don't hang.
 	select {
-	case <-ctx.Done():
-		return util.WrapCtxErrWithCause(ctx)
 	case ch <- val:
 		return nil
+	default:
+	}
+
+	// 2. We couldn't send immediately, so now we wait for either
+	// the channel to become ready OR the context to be canceled.
+	select {
+	case ch <- val:
+		return nil
+	case <-ctx.Done():
+		return util.WrapCtxErrWithCause(ctx)
 	}
 }
--- internal/verifier/compare_test.go
+++ internal/verifier/compare_test.go
@@ -2,19 +2,180 @@ package verifier
 
 import (
 	"context"
-	"math/rand"
+	"math/rand/v2"
+	"strconv"
+	"strings"
 	"sync/atomic"
 	"time"
 
 	"github.com/10gen/migration-verifier/contextplus"
 	"github.com/10gen/migration-verifier/internal/partitions"
+	"github.com/10gen/migration-verifier/internal/types"
 	"github.com/10gen/migration-verifier/internal/verifier/tasks"
 	"github.com/10gen/migration-verifier/mslices"
 	"github.com/mongodb-labs/migration-tools/bsontools"
 	"github.com/pkg/errors"
 	"github.com/samber/lo"
 	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
+
+func (s *IntegrationTestSuite) TestFetchAndCompareDocuments_ManySmallProblems() {
+	ctx := s.Context()
+
+	srcColl := s.srcMongoClient.Database(s.DBNameForTest()).Collection("stuff")
+
+	docsCount := 0
+
+	batchesCount := 1_000
+	perBatchCount := 1_000
+
+	for range batchesCount {
+		res, err := srcColl.InsertMany(
+			ctx,
+			mslices.Map1(
+				lo.Range(perBatchCount),
+				func(i int) bson.D {
+					return bson.D{
+						{"_id", docsCount + i},
+					}
+				},
+			),
+			options.InsertMany().SetOrdered(false),
+		)
+
+		s.Require().NoError(err)
+
+		docsCount += len(res.InsertedIDs)
+	}
+
+	s.Require().NoError(
+		s.dstMongoClient.Database(s.DBNameForTest()).CreateCollection(
+			ctx,
+			srcColl.Name(),
+		),
+	)
+
+	task := tasks.Task{
+		PrimaryKey: bson.NewObjectID(),
+		Type:       tasks.VerifyDocuments,
+		Status:     tasks.Processing,
+		QueryFilter: tasks.QueryFilter{
+			Namespace: s.DBNameForTest() + ".stuff",
+			Partition: &partitions.Partition{
+				Key: partitions.PartitionKey{
+					Lower: bsontools.ToRawValue(bson.MinKey{}),
+				},
+				Upper: bsontools.ToRawValue(bson.MaxKey{}),
+			},
+		},
+	}
+
+	verifier := s.BuildVerifier()
+	s.Require().NoError(verifier.startChangeHandling(ctx))
+
+	results := lo.ChannelToSlice(verifier.FetchAndCompareDocuments(
+		ctx,
+		0,
+		&task,
+	))
+
+	s.Assert().Greater(len(results), 1, "need multiple reports")
+
+	var problemsCount int
+	var totalDocs types.DocumentCount
+
+	for _, result := range results {
+		report, err := result.Get()
+		s.Require().NoError(err)
+
+		problemsCount += len(report.Problems)
+		totalDocs += report.DocCount
+	}
+
+	s.Assert().EqualValues(batchesCount*perBatchCount, problemsCount, "total problems")
+	s.Assert().EqualValues(batchesCount*perBatchCount, totalDocs, "total docs checked")
+}
+
+func (s *IntegrationTestSuite) TestFetchAndCompareDocuments_BigProblems() {
+	ctx := s.Context()
+
+	srcColl := s.srcMongoClient.Database(s.DBNameForTest()).Collection("stuff")
+
+	docsCount := 0
+
+	batchesCount := 100
+	perBatchCount := 10
+
+	for range batchesCount {
+		res, err := srcColl.InsertMany(
+			ctx,
+			mslices.Map1(
+				lo.Range(perBatchCount),
+				func(i int) bson.D {
+					return bson.D{
+						{"_id", strings.Repeat(
+							strconv.Itoa(docsCount+i),
+							10_000,
+						)},
+					}
+				},
+			),
+			options.InsertMany().SetOrdered(false),
+		)
+
+		s.Require().NoError(err)
+
+		docsCount += len(res.InsertedIDs)
+	}
+
+	s.Require().NoError(
+		s.dstMongoClient.Database(s.DBNameForTest()).CreateCollection(
+			ctx,
+			srcColl.Name(),
+		),
+	)
+
+	task := tasks.Task{
+		PrimaryKey: bson.NewObjectID(),
+		Type:       tasks.VerifyDocuments,
+		Status:     tasks.Processing,
+		QueryFilter: tasks.QueryFilter{
+			Namespace: s.DBNameForTest() + ".stuff",
+			Partition: &partitions.Partition{
+				Key: partitions.PartitionKey{
+					Lower: bsontools.ToRawValue(bson.MinKey{}),
+				},
+				Upper: bsontools.ToRawValue(bson.MaxKey{}),
+			},
+		},
+	}
+
+	verifier := s.BuildVerifier()
+	s.Require().NoError(verifier.startChangeHandling(ctx))
+
+	results := lo.ChannelToSlice(verifier.FetchAndCompareDocuments(
+		ctx,
+		0,
+		&task,
+	))
+
+	s.Assert().Greater(len(results), 1, "need multiple reports")
+
+	var problemsCount int
+	var totalDocs types.DocumentCount
+
+	for _, result := range results {
+		report, err := result.Get()
+		s.Require().NoError(err)
+
+		problemsCount += len(report.Problems)
+		totalDocs += report.DocCount
+	}
+
+	s.Assert().EqualValues(batchesCount*perBatchCount, problemsCount, "total problems")
+	s.Assert().EqualValues(batchesCount*perBatchCount, totalDocs, "total docs checked")
+}
 
 // TestFetchAndCompareDocuments_Context ensures that nothing hangs
 // when a context is canceled during FetchAndCompareDocuments().
@@ -64,6 +225,8 @@ func (s *IntegrationTestSuite) TestFetchAndCompareDocuments_Context() {
 				&task,
 			))
 
+			done.Store(true)
+
 			s.Require().Len(reports, 1)
 
 			_, err := reports[0].Get()
@@ -75,7 +238,6 @@ func (s *IntegrationTestSuite) TestFetchAndCompareDocuments_Context() {
 					"only failure should be context cancellation",
 				)
 			}
-			done.Store(true)
 		}()
 
 		delay := time.Duration(100 * float64(time.Millisecond) * rand.Float64())
