0615d84879a3e513db9e281b6ed66e867d0c48fb
Felipe Gasper <felipe@felipegasper.com>
2026-03-11 16:18:28 -0400

    debug

--- internal/verifier/summary.go
+++ internal/verifier/summary.go
@@ -157,7 +157,6 @@ func (verifier *Verifier) reportDocumentMismatches(ctx context.Context, strBuild
 
 		for _, m := range reportData.ContentDiffers {
 			if m.Detail.DocumentIsMissing() {
-				fmt.Printf("------- report data:\n%+v\n\n", reportData)
 				panic(fmt.Sprintf("found missing-type mismatch but expected content-differs: %+v", m))
 			}
 
