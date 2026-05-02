149fe2154cbdde9575a5cf17a205121b77537ce0
Felipe Gasper <felipe@felipegasper.com>
2026-05-02 06:20:47 -0400

    add comment

--- internal/verifier/api/change_event_counts.go
+++ internal/verifier/api/change_event_counts.go
@@ -5,6 +5,8 @@ import "github.com/rs/zerolog"
 // ChangeEventCounts tallies cumulative change events seen by a change reader,
 // across all generations, since the verifier first started.
 type ChangeEventCounts struct {
+	// NB:  These are int64 because they get serialized to extJSON.
+	// The API implementation’s Go types are unfortunately inconsistent.
 	Insert  int64
 	Update  int64
 	Replace int64
